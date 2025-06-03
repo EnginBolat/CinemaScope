5e2371d4217df881d3df4855fc4d8c1459f2cd86
Engin Bolat <engi.bolat@gmail.com>
2025-06-03 22:40:28 +0300

    feat: added tabswitdh

---
+++ src/components/TabSwitch/types.ts
@@ -0,0 +1,14 @@
+export type TabButtonType = {
+  title: string;
+};
+
+export type ITabSwitch = {
+  buttons: TabButtonType[];
+  selectedTab: number;
+  setSelectedTab: (tab: number) => void;
+};
+
+export type IDimensions = {
+  height: number;
+  width: number;
+};
